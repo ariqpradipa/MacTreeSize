e874f7ac4e32ec422fa9cb5afe95e2dd6d31da4f
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2026-01-19 11:14:14 +0700

    Initial Commit

---
+++ MacTreeSize/MacTreeSizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTreeSizeApp.swift
+//  MacTreeSize
+//
+//  Created by encore on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacTreeSizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
